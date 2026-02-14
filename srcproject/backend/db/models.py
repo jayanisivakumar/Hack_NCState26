caed7302ce5ce6342f25e9694892c74822e970eb
spanja05 <simone.panja@gmail.com>
2026-02-14 14:33:18 -0500

    connection to database

--- srcproject/backend/db/models.py
+++ srcproject/backend/db/models.py
@@ -1,49 +1,48 @@
-from sqlalchemy import create_engine, Column, String, Integer, Text, ForeignKey
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker, relationship
-from sqlalchemy.exc import IntegrityError
-from dotenv import load_dotenv
-import os
+from sqlalchemy import Column, Integer, String, Text, ForeignKey, Boolean
+from sqlalchemy.orm import relationship
+from database import Base
 
-load_dotenv()
-DATABASE_URL = os.getenv("DATABASE_URL")
-engine = create_engine(DATABASE_URL)
-Base = declarative_base()
-Session = sessionmaker(bind=engine)
-session = Session()
-
-# Models
 class User(Base):
     __tablename__ = "users"
+
     id = Column(Integer, primary_key=True)
     username = Column(String, nullable=False)
     password = Column(String, nullable=False)
     email = Column(String, nullable=False, unique=True)
-    folders = relationship('Folder', back_populates='users', cascade="all, delete-orphan")
+
+    folders = relationship("Folder", back_populates="user", cascade="all, delete-orphan")
+
 
 class Folder(Base):
     __tablename__ = "folder"
+
     folder_id = Column(Integer, primary_key=True)
-    user_id = Column(Integer, ForeignKey('users.id', ondelete="CASCADE"))
+    user_id = Column(Integer, ForeignKey("users.id", ondelete="CASCADE"), nullable=False)
     folder_name = Column(String, nullable=False)
-    user = relationship('User', back_populates='folder')
-    notes = relationship('Note', back_populates='folder', cascade="all, delete-orphan")
+
+    user = relationship("User", back_populates="folders")
+    notes = relationship("Note", back_populates="folder", cascade="all, delete-orphan")
+
 
 class Note(Base):
     __tablename__ = "note"
+
     note_id = Column(Integer, primary_key=True)
-    folder_id = Column(Integer, ForeignKey('folder.folder_id', ondelete="CASCADE"))
+    folder_id = Column(Integer, ForeignKey("folder.folder_id", ondelete="CASCADE"), nullable=False)
     name = Column(String, nullable=False)
     notes = Column(Text, nullable=False)
-    folder = relationship('Folder', back_populates='note')
-    flashcards = relationship('Flashcard', back_populates='note', cascade="all, delete-orphan")
+
+    folder = relationship("Folder", back_populates="notes")
+    flashcards = relationship("Flashcard", back_populates="note", cascade="all, delete-orphan")
+
 
 class Flashcard(Base):
     __tablename__ = "flashcard"
+
     flashcard_id = Column(Integer, primary_key=True)
-    note_id = Column(Integer, ForeignKey('note.note_id', ondelete="CASCADE"))
+    note_id = Column(Integer, ForeignKey("note.note_id", ondelete="CASCADE"), nullable=False)
     term = Column(String, nullable=False)
     definition = Column(Text, nullable=False)
-    note = relationship('Note', back_populates='flashcard')
+    user_generated = Column(Boolean, default=False)
 
-Base.metadata.create_all(engine)
+    note = relationship("Note", back_populates="flashcards")
